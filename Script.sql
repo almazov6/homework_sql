30c541bad287290917f6aae686c0804270350c6b
almazov <almazov6@ya.ru>
2024-09-23 22:03:53 +0300

    add sql

---
+++ Script.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS Artist (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR (100) NOT null
+);
+
+CREATE TABLE IF NOT EXISTS Ganre (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR (100) NOT null
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR (100) NOT NULL, 
+	year_of_release DATE NOT null
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR (100) NOT NULL, 
+	duration INTEGER not null,
+	album_id INTEGER NOT NULL REFERENCES Album(id)
+);
+
+CREATE TABLE IF NOT EXISTS Collection (
+	id SERIAL PRIMARY key,
+	name VARCHAR (100) NOT NULL, 
+	year_of_release DATE NOT null
+);
+
+CREATE TABLE IF NOT EXISTS ArtistGenre (
+	id SERIAL PRIMARY key,
+	artist_id INTEGER NOT NULL REFERENCES Artist(id),
+	ganre_id INTEGER NOT NULL REFERENCES  Ganre(id)
+);
+
+CREATE TABLE IF NOT EXISTS ArtistAlbum (
+	id SERIAL PRIMARY key,
+	artist_id INTEGER NOT NULL REFERENCES Artist(id),
+	album_id INTEGER NOT NULL REFERENCES  Album(id)
+);
+
+CREATE TABLE IF NOT EXISTS TrackCollection (
+	id SERIAL PRIMARY key,
+	track_id INTEGER NOT NULL REFERENCES Track(id),
+	collection_id INTEGER NOT NULL REFERENCES  Collection(id)
+);
+
